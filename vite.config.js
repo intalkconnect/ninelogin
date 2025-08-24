cd1a91790b94904cc5573b75f71c04d099166e4e
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-08-24 16:04:06 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: 3300,
+    allowedHosts: [
+      'portal.dkdevs.com.br',   // libera esse domínio
+      'srv-auth.dkdevs.com.br'  // adicione outros se precisar
+    ]
+  }
 })
