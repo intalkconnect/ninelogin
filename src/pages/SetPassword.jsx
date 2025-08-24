f9fe3a7c928780533070d22b4c5a49b7694a2002
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-08-24 16:57:40 -0300

    Update SetPassword.jsx

--- src/pages/SetPassword.jsx
+++ src/pages/SetPassword.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import '../Login.css';
-import logo from './assets/ninechat_logo_icons.png';
+import logo from '../assets/ninechat_logo_icons.png';
 import { Link } from 'react-router-dom';
 
 // ==== BASE DO BACKEND (mesma lógica do Login.jsx) ====
