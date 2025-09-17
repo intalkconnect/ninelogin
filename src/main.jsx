426a10a9712c2529fbb6d572a975e1f482c1b503
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-09-17 19:58:43 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,15 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Login from './Login';
 import ForgotPassword from './pages/ForgotPassword';
 import SetPassword from './pages/SetPassword';
+import PrivacyPolicy from "./legal/PrivacyPolicy";
+import TermsOfUse from "./legal/TermsOfUse";
 
 const router = createBrowserRouter([
   { path: '/', element: <Login /> },
   { path: '/auth/forgot-password', element: <ForgotPassword /> },
   { path: '/auth/set-password', element: <SetPassword /> },
+  { path: '/legal/privacy', element: <PrivacyPolicy /> },
+  { path: '/legal/terms', element: <TermsOfUse /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
